726ac50c031960dffeba5cbf778f700fa87c10ec
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-04-04 15:17:46 +0200

    Initial commit

---
+++ src/Gibspiel.java
@@ -0,0 +1,27 @@
+public class Gibspiel {
+
+    /*
+     * Ein Spiel für 2 Spieler.
+     * Jeder Spieler kann 1-10 Elemente auf einem Spielbrett plazieren,
+     * selbst gewählt, mindestens eins, maximal 10 Elemente.
+     * Welcher Spieler das 100. Element hinlegt, hat gewonnen.
+     * Das Spiel beginnt mit einer zufälligen Anzahl von Elementen (< 30),
+     * die bereits auf dem Spielbrett liegen.
+     *
+     * Benutzen Sie das Konzept der Vererbung, um wahlweise einen
+     * menschlichen oder computergesteuerten Spieler teilnehmen zu lassen
+     * (0, 1 oder 2 Computerspieler).
+     *
+     * Verwenden Sie am Anfang eine schwache KI für den computergesteuerten
+     * Spieler.
+     *
+     * Notwendige Klassen:
+     * - Spieler
+     *   - MenschSpieler
+     *   - ComputerSpieler
+     * - Spielbrett
+     */
+    public static void main(String[] args) {
+
+    }
+}
